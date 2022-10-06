fa7b1c0a288a3a4709a789a7a9f444f21f620099
Sean Hasenstein <seanhasenstein@gmail.com>
2022-10-05 22:17:21 -0500

    fix bug with status state on component mount

--- pages/send-email.tsx
+++ pages/send-email.tsx
@@ -24,7 +24,7 @@ const groups = [
 export default function SendEmail() {
   const session = useSession();
   const [status, setStatus] = React.useState<'idle' | 'error' | 'success'>(
-    'error'
+    'idle'
   );
 
   const handleSubmit = async (formValues: SendEmailFormValues) => {
